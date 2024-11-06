1059616f42d65547b2c8260a03a79a2bee1abbf2
melomatheuss <matheus.syn.eco@gmail.com>
2024-11-05 23:12:54 -0400

    Resolved merge conflicts

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+import { styles } from "./style"
+
+
+export default function HomeScreen(){
+    return(
+        <View style = {styles.container}>
+            <Text style = {styles.textHome}>
+                kevin cornokkkkk
+            </Text>
+        </View>
+
+    )
+}
